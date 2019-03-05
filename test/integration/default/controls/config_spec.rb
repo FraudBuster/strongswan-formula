21e7e6b46916ca8bf1f7556dc63885448a54e65a
Eric Veiras Galisson <eric.veiras-galisson@fraudbuster.mobi>
2019-03-05 13:22:15 +0100

    config tests for one full connection file

--- test/integration/default/controls/config_spec.rb
+++ test/integration/default/controls/config_spec.rb
@@ -18,6 +18,7 @@ control 'Strongswan configuration' do
     it { should be_grouped_into 'root' }
   end
 
+  # we verify presence of all connection files
   ['conn_a', 'conn_b'].each do |conn|
     describe file("/etc/ipsec.conf.d/#{conn}.conf") do
       it { should be_file }
@@ -28,4 +29,15 @@ control 'Strongswan configuration' do
     end
   end
 
+  # but we only verify the full content of one
+  describe file('/etc/ipsec.conf.d/conn_a.conf') do
+    its('content') { should include '    authby=secret' }
+    its('content') { should include '    auto=add' }
+    its('content') { should include '    esp=3des-sha1' }
+    its('content') { should include '    ike=3des-sha1-modp1024' }
+    its('content') { should include '    left=10.10.1.1' }
+    its('content') { should include '    leftsubnet=10.10.1.0/32' }
+    its('content') { should include '    right=10.20.1.1' }
+    its('content') { should include '    rightsubnet=10.20.1.0/32' }
+  end
 end
