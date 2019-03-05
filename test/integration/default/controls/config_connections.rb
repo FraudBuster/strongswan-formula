fdc6f53bcaad14b99c292bfd3aa814192e137d71
Eric Veiras Galisson <eric.veiras-galisson@fraudbuster.mobi>
2019-03-05 13:35:29 +0100

    reorganized Inspec tests

--- test/integration/default/controls/config_connections.rb
+++ test/integration/default/controls/config_connections.rb
@@ -1,16 +1,5 @@
-control 'Strongswan configuration' do
+control 'Strongswan connections configuration' do
   title 'should match desired lines'
-
-  describe file('/etc/ipsec.conf') do
-    it { should be_file }
-    it { should be_owned_by 'root' }
-    it { should be_grouped_into 'root' }
-    its('mode') { should cmp '0644' }
-    its('content') { should include 'conn setup' }
-    its('content') { should include '   strictcrlpolicy=yes' }
-    its('content') { should include '   uniqueids=no' }
-    its('content') { should include 'include /etc/ipsec.conf.d/*.conf' }
-  end
 
   describe directory('/etc/ipsec.conf.d') do
     it { should be_directory }
---
+++ test/integration/default/controls/config_global.rb
@@ -0,0 +1,14 @@
+control 'Strongswan global configuration' do
+  title 'should match desired lines'
+
+  describe file('/etc/ipsec.conf') do
+    it { should be_file }
+    it { should be_owned_by 'root' }
+    it { should be_grouped_into 'root' }
+    its('mode') { should cmp '0644' }
+    its('content') { should include 'conn setup' }
+    its('content') { should include '   strictcrlpolicy=yes' }
+    its('content') { should include '   uniqueids=no' }
+    its('content') { should include 'include /etc/ipsec.conf.d/*.conf' }
+  end
+end
