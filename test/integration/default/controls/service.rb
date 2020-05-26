01b816f54525a6635b2708fc85de4a78856ef829
Eric Veiras Galisson <eric.veiras-galisson@fraudbuster.mobi>
2020-05-26 19:02:37 +0200

    ci(inspec): update ubuntu 2004 service tests

--- test/integration/default/controls/service.rb
+++ test/integration/default/controls/service.rb
@@ -1,9 +1,14 @@
 # frozen_string_literal: true
 
+service_name = 'strongswan'
+if (os[:name] == 'ubuntu') && os[:release].start_with?('20')
+  service_name = 'strongswan-starter'
+end
+
 control 'Strongswan service' do
   title 'should be running and enabled'
 
-  describe service('strongswan') do
+  describe service(service_name) do
     it { should be_installed }
     it { should be_enabled }
     it { should be_running }
