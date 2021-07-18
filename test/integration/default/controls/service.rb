038eade761806a327d35cc299e017e2a6780ed90
Imran Iqbal <iqbalmy@hotmail.com>
2021-07-18 14:10:40 +0100

    feat(osfingermap): add support for `debian-11` [skip ci]

--- test/integration/default/controls/service.rb
+++ test/integration/default/controls/service.rb
@@ -1,9 +1,12 @@
 # frozen_string_literal: true
 
-service_name = 'strongswan'
-if (os[:name] == 'ubuntu') && os[:release].start_with?('20')
-  service_name = 'strongswan-starter'
-end
+service_name =
+  case system.platform[:finger]
+  when 'ubuntu-20.04', 'debian-11'
+    'strongswan-starter'
+  else
+    'strongswan'
+  end
 
 control 'Strongswan service' do
   title 'should be running and enabled'
